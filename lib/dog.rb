f7d5f5ca308a71384642a15700a4cbf2e97c3ffc
Kirby Campbell <jkirbycampbell@gmail.com>
2018-04-17 07:11:44 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,13 +1,12 @@
 # Your code goes here!
 class Dog
 
-def name=(dog_name)
-  this_dogs_name = dog_name
-end
+
 
 def name
-  this_dogs_name
+  name = "Fido"
 end
+
   def bark
     puts "woof!"
   end
